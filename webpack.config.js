2f10ef3faac4cf2c29a1a0f536a6e5c6ca36ba0d
konstantin vlasov <flockast@gmail.com>
2018-10-25 11:40:07 +0300

    add index.ejs && update webpack-html-plugin && minify html

--- webpack.config.js
+++ webpack.config.js
@@ -2,79 +2,77 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-    entry: [
-        './src/js/index.js',
-        './src/styles/index.scss'
-    ],
-    output: {
-        path: path.resolve(__dirname, 'prod'),
-        filename: 'js/[name].bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.m?js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
+module.exports = env => {
+    const isDev = env.mode === "development";
+
+    return {
+        entry: [
+            './src/js/index.js',
+            './src/styles/index.scss'
+        ],
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'js/[name].bundle.js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.m?js$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
                     }
+                },
+                {
+                    test: /\.(scss|sass)$/,
+                    use: ExtractTextPlugin.extract({
+                        fallback: 'style-loader',
+                        use: ['css-loader', 'sass-loader'],
+                        publicPath: '../'
+                    })
+                },
+                {
+                    test: /\.(png|jpg|gif)$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                context: './src/img',
+                                name: '[name].[ext]',
+                                outputPath: 'img/'
+                            }
+                        }
+                    ]
+                },
+                {
+                    test: /\.(ttf|eot|woff|woff2)$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                context: './src/fonts',
+                                name: '[name].[ext]',
+                                outputPath: 'fonts/'
+                            }
+                        }
+                    ]
                 }
-            },
-            {
-                test: /\.(scss|sass)$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: ['css-loader', 'sass-loader'],
-                    publicPath: '../'
-                })
-            },
-            {
-                test: /\.(png|jpg|gif)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name].[ext]',
-                            outputPath: 'img/'
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.(ttf|eot|woff|woff2)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name].[ext]',
-                            outputPath: 'fonts/'
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: 'html-loader',
-                        options: {
-                            minimize: true
-                        }
-                    }
-                ],
-            }
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                filename: 'index.html',
+                template: './src/index.ejs',
+                minify: {
+                    collapseWhitespace: !isDev
+                }
+            }),
+            new ExtractTextPlugin({
+                filename: 'css/[name].bundle.css',
+            })
         ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: './src/index.html'
-        }),
-        new ExtractTextPlugin({
-            filename: 'css/[name].bundle.css',
-        })
-    ]
+    }
 };
