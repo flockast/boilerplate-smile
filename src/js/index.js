62a55e8bb4374a3f5d81541f5294dd27964eab00
konstantin vlasov <flockast@gmail.com>
2018-12-28 12:52:28 +0300

    js refactor

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,3 @@
 // ES6
-const sum = (...numbers) => numbers.reduce((sum, current) => sum + current)
-console.log(sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+const sum = (...numbers) => numbers.reduce((sum, current) => sum + current);
+console.log(sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
