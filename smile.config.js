5f518406739130ae7e098a13ec951d545d905ef5
konstantin vlasov <k.vlasov@game-insight.com>
2019-08-22 16:56:19 +0300

    config in in single file

---
+++ smile.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+	"src": {
+		"base": "./src",
+		"styles": "styles/index.scss",
+		"js": "js/index.js",
+		"views": "views"
+	},
+	"build": {
+		"base": "./dist",
+		"styles": "bundle[hash].css",
+		"js": "bundle[hash].js"
+	},
+	"copy": [
+		{
+			"from": "favicon.ico",
+			"to": "[name].[ext]"
+		}
+	],
+	"routes": [
+		{
+			"url": "/",
+			"file": "pages/home.njk"
+		},
+		{
+			"url": "about",
+			"file": "pages/about.njk"
+		}
+	],
+	"publicPath": ""
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 const path = require('path');
-const glob = require("glob");
+const glob = require('glob');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const {CleanWebpackPlugin}  = require('clean-webpack-plugin');
-const PurgecssPlugin = require('purgecss-webpack-plugin');
-const {smile} = require('./package.json');
+const PurgeCssPlugin = require('purgecss-webpack-plugin');
+const smile = require('./smile.config.js');
 
 module.exports = (env, options) => {
 
@@ -59,12 +59,15 @@ module.exports = (env, options) => {
                 },
                 {
                     test: /\.(njk|nunjucks|nunj)$/,
-                    use: ['html-loader', {
-                        loader: 'nunjucks-html-loader',
-                        options : {
-                            searchPaths: [`${smile.src.base}/${smile.src.views}`]
+                    use: [
+                        'html-loader',
+                        {
+                            loader: 'nunjucks-html-loader',
+                            options : {
+                                searchPaths: [`${smile.src.base}/${smile.src.views}`]
+                            }
                         }
-                    }]
+                    ]
                 },
                 {
                     test: /\.(png|jpg|gif|ttf|woff|woff2|eot|otf|svg)$/,
@@ -92,7 +95,7 @@ module.exports = (env, options) => {
         plugins: [
             !isDev ? new OptimizeCSSAssetsPlugin({}) : () => {},
             !isDev ? new CleanWebpackPlugin({}) : () => {},
-            !isDev ? new PurgecssPlugin({
+            !isDev ? new PurgeCssPlugin({
                 paths: glob.sync(`${smile.src.base}/${smile.src.views}/**/*`, { nodir: true })
             }) : () => {},
             new CopyWebpackPlugin(copyFiles),
